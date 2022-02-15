2c7c6b598b2a1dff61cd5379922cca7e77a371ce
anwarul1 <anwarjewel007@gmail.com>
2022-02-15 23:46:10 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,51 @@
+// document.getElementById('sixteenGB').addEventListener('click', function () {
+//   const memoryCost = document.getElementById('memory-cost');
+//   memoryCost.innerText = 200;
+//   const bastPrice = document.getElementById('best-price').innerText;
+//   const memoryPrice = document.getElementById('memory-cost').innerText;
+//   const storagePrice = document.getElementById('storage-cost').innerText;
+//   const deliveryPrice = document.getElementById('delivery-cost').innerText;
+//   const totalPrice =
+//     parseFloat(bastPrice) +
+//     parseFloat(memoryPrice) +
+//     parseFloat(storagePrice) +
+//     parseFloat(deliveryPrice);
+//   const total = document.getElementById('total-price');
+//   total.innerText = totalPrice;
+//function call// updatePrice('memory-cost', 300);
+// });
+
+document.getElementById('sixteenGB').addEventListener('click', function () {
+  updatePrice('memory-cost', 300);
+});
+document.getElementById('eightGB').addEventListener('click', function () {
+  updatePrice('memory-cost', 0);
+});
+document.getElementById('ssd3').addEventListener('click', function () {
+  updatePrice('storage-cost', 800);
+});
+document.getElementById('ssd2').addEventListener('click', function () {
+  updatePrice('storage-cost', 500);
+});
+document.getElementById('ssd1').addEventListener('click', function () {
+  updatePrice('storage-cost', 0);
+});
+
+function updatePrice(itemId, price) {
+  const memoryCost = document.getElementById(itemId);
+  memoryCost.innerText = price;
+
+  const bastPrice = document.getElementById('best-price').innerText;
+  const memoryPrice = document.getElementById('memory-cost').innerText;
+  const storagePrice = document.getElementById('storage-cost').innerText;
+  const deliveryPrice = document.getElementById('delivery-cost').innerText;
+
+  const totalPrice =
+    parseFloat(bastPrice) +
+    parseFloat(memoryPrice) +
+    parseFloat(storagePrice) +
+    parseFloat(deliveryPrice);
+
+  const total = document.getElementById('total-price');
+  total.innerText = totalPrice;
+}
