ba0a2ab6e489d161948e1a638e87c0dd085b812e
anwarul1 <anwarjewel007@gmail.com>
2022-02-16 04:33:14 +0600

    error massage done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//----------------------->
 // document.getElementById('sixteenGB').addEventListener('click', function () {
 //   const memoryCost = document.getElementById('memory-cost');
 //   memoryCost.innerText = 200;
@@ -14,6 +15,8 @@
 //   total.innerText = totalPrice;
 //function call// updatePrice('memory-cost', 300);
 // });
+
+//----------------------->
 /* 
 document.getElementById('sixteenGB').addEventListener('click', function () {
   updatePrice('memory-cost', 300);
@@ -37,7 +40,9 @@ document.getElementById('free-delivery').addEventListener('click', function () {
   updatePrice('delivery-cost', 0);
 });
  */
-/* function updatePrice(itemId, price) {
+//----------------------->
+/* 
+function updatePrice(itemId, price) {
   const memoryCost = document.getElementById(itemId);
   memoryCost.innerText = price;
 
@@ -54,9 +59,10 @@ document.getElementById('free-delivery').addEventListener('click', function () {
 
   const total = document.getElementById('total-price');
   total.innerText = totalPrice;
-} */
+} 
+*/
 
-function updatePrice(itemId, price, clickId) {
+function updatePrice(clickId, itemId, price) {
   document.getElementById(clickId).addEventListener('click', function () {
     const memoryCost = document.getElementById(itemId);
     memoryCost.innerText = price;
@@ -76,10 +82,43 @@ function updatePrice(itemId, price, clickId) {
     total.innerText = totalPrice;
   });
 }
-updatePrice('memory-cost', 300, 'sixteenGB');
-updatePrice('memory-cost', 0, 'eightGB');
-updatePrice('storage-cost', 800, 'ssd3');
-updatePrice('storage-cost', 500, 'ssd2');
-updatePrice('storage-cost', 0, 'ssd1');
-updatePrice('delivery-cost', 20, 'paid-delivery');
-updatePrice('delivery-cost', 0, 'free-delivery');
+updatePrice('sixteenGB', 'memory-cost', 300);
+updatePrice('eightGB', 'memory-cost', 0);
+updatePrice('ssd3', 'storage-cost', 800);
+updatePrice('ssd2', 'storage-cost', 500);
+updatePrice('ssd1', 'storage-cost', 0);
+updatePrice('paid-delivery', 'delivery-cost', 20);
+updatePrice('free-delivery', 'delivery-cost', 0);
+
+const fakeCode = 'pHero';
+
+document.getElementById('apply-btn').addEventListener('click', function () {
+  const code = document.getElementById('promo-input');
+  const codeValue = code.value;
+  const sussesMassage = document.getElementById('applied-text');
+  const errorMessage = document.getElementById('not-applied-text');
+  if (codeValue === fakeCode) {
+    const total = document.getElementById('total-price');
+    let totalPrice = parseFloat(total.innerText);
+    const discount = totalPrice * 0.2;
+    totalPrice = totalPrice - discount;
+    total.innerText = totalPrice;
+    // massage handling for susses massage
+    sussesMassage.classList.remove('d-none');
+    sussesMassage.classList.add('d-block');
+    errorMessage.classList.add('d-none');
+  } else {
+    // massage handling for  error massage
+    sussesMassage.classList.add('d-none');
+    errorMessage.classList.remove('d-none');
+    errorMessage.classList.add('d-block');
+  }
+
+  // clear the field
+  code.value = '';
+});
+
+// document.getElementById('apply-btn').addEventListener('click', function () {
+//   const sussesMassage = document.getElementById('applied-text');
+//   console.log(sussesMassage.innerText);
+// });
